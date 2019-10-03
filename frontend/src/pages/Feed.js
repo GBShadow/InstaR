8179b6674983559b94d619cfe3099d96eb63797d
GBShadow <gbs.shadow@gmail.com>
2019-10-03 17:55:08 -0300

    Styled Feed

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,11 +1,71 @@
 import React, { Component } from 'react';
+import api from "../services/api";
+
+import './Feed.css';
+
+import more from '../assets/more.svg'; 
+import like from '../assets/like.svg'; 
+import comment from '../assets/comment.svg'; 
+import send from '../assets/send.svg'; 
 
 class Feed extends Component {
-    render(){
-        return (
-        <h1>Feed</h1>
-        );
-    }
+render(){
+return (
+<section id="post-list">
+    <article>
+        <header>
+            <div class="user-info">
+                <span>Gustavo</span>
+                <span class="place">Cidade Jardim</span>
+            </div>
+
+            <img src={more} alt="Mais" />
+        </header>
+        <img src="http://localhost:3333/files/blastoise-pictures-click-to-edit-cool-blastoise-pictures.jpg" alt="" />
+
+        <footer>
+            <div class="actions">
+                <img src={like} alt="Likes" />
+                <img src={comment} alt="Comment" />
+                <img src={send} alt="Enviar" />
+            </div>
+
+            <strong>900 Curtidas</strong>
+            <p>
+                Um post Muito legal
+                <span>#Blastoise</span>
+            </p>
+        </footer>
+    </article>
+
+    <article>
+            <header>
+                <div class="user-info">
+                    <span>Gustavo</span>
+                    <span class="place">Cidade Jardim</span>
+                </div>
+    
+                <img src={more} alt="Mais" />
+            </header>
+            <img src="http://localhost:3333/files/blastoise-pictures-click-to-edit-cool-blastoise-pictures.jpg" alt="" />
+    
+            <footer>
+                <div class="actions">
+                    <img src={like} alt="Likes" />
+                    <img src={comment} alt="Comment" />
+                    <img src={send} alt="Enviar" />
+                </div>
+    
+                <strong>900 Curtidas</strong>
+                <p>
+                    Um post Muito legal
+                    <span>#Blastoise</span>
+                </p>
+            </footer>
+        </article>
+</section>
+);
+}
 }
 
 export default Feed;
