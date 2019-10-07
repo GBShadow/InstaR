f573445bb7a142c73b42e269a7a25eeeadd69e8b
GBShadow <gbs.shadow@gmail.com>
2019-10-07 12:04:40 -0300

    Styled New Post

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -3,69 +3,55 @@ import api from "../services/api";
 
 import './Feed.css';
 
-import more from '../assets/more.svg'; 
-import like from '../assets/like.svg'; 
-import comment from '../assets/comment.svg'; 
-import send from '../assets/send.svg'; 
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
 
 class Feed extends Component {
-render(){
-return (
-<section id="post-list">
-    <article>
-        <header>
-            <div class="user-info">
-                <span>Gustavo</span>
-                <span class="place">Cidade Jardim</span>
-            </div>
+    state = {
+        feed: [],
+    };
 
-            <img src={more} alt="Mais" />
-        </header>
-        <img src="http://localhost:3333/files/blastoise-pictures-click-to-edit-cool-blastoise-pictures.jpg" alt="" />
+    async componentDidMount() {
+        const response = await api.get("posts");
 
-        <footer>
-            <div class="actions">
-                <img src={like} alt="Likes" />
-                <img src={comment} alt="Comment" />
-                <img src={send} alt="Enviar" />
-            </div>
+        this.setState({ feed: response.data });
+    }
 
-            <strong>900 Curtidas</strong>
-            <p>
-                Um post Muito legal
-                <span>#Blastoise</span>
-            </p>
-        </footer>
-    </article>
+    render() {
+        return (
+            <section id="post-list">
+                {this.state.feed.map(post => (
+                    <article key={post._id}>
+                    <header>
+                        <div class="user-info">
+                            <span>{post.author}</span>
+                            <span class="place">{post.place}</span>
+                        </div>
 
-    <article>
-            <header>
-                <div class="user-info">
-                    <span>Gustavo</span>
-                    <span class="place">Cidade Jardim</span>
-                </div>
-    
-                <img src={more} alt="Mais" />
-            </header>
-            <img src="http://localhost:3333/files/blastoise-pictures-click-to-edit-cool-blastoise-pictures.jpg" alt="" />
-    
-            <footer>
-                <div class="actions">
-                    <img src={like} alt="Likes" />
-                    <img src={comment} alt="Comment" />
-                    <img src={send} alt="Enviar" />
-                </div>
-    
-                <strong>900 Curtidas</strong>
-                <p>
-                    Um post Muito legal
-                    <span>#Blastoise</span>
-                </p>
-            </footer>
-        </article>
-</section>
-);
-}
+                        <img src={more} alt="Mais" />
+                    </header>
+                    <img src={`http://localhost:3333/files/${post.image}`} alt="" />
+
+                    <footer>
+                        <div class="actions">
+                            <img src={like} alt="Likes" />
+                            <img src={comment} alt="Comment" />
+                            <img src={send} alt="Enviar" />
+                        </div>
+
+                        <strong>{post.likes} Curtidas</strong>
+                        <p>
+                            {post.description}
+                <span>{post.hashtags}</span>
+                        </p>
+                    </footer>
+                </article>
+                ))}
+            </section>
+        );
+    }
 }
 
 export default Feed;
